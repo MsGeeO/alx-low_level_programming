801bbd53fdacacda0448db2f3fa1896cef00db7a
MsGeeO <geethatcher@gmail.com>
2022-11-02 14:42:46 -0700

    update

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -3,12 +3,13 @@
 /**
  * _rev_recursion - prints string in reverse
  * @s: string to be printed
+ * Return: Always 0 (success)
  */
 void _print_rev_recursion(char *s)
 {
-	if (*s)
-	{
+	if (*s != '\0')
 		_print_rev_recursion(s + 1);
-		_putchar(*s);
-	}
+	else
+		return;
+	_putchar(*s);
 }
--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -4,15 +4,12 @@
  * _strlen_recursion - Returns the length of a string
  * @s: string to be measured
  *
- * Return: The length of the string
+ * Return: Always 0 (success)
  */
 int _strlen_recursion(char *s)
 {
-	int len = 0;
-
-	if (*s)
-	{
-		len++;
-		len += _strlen_recursion(s + 1)
-	}
+	if (*s == '\0')
+		return (0);
+	else
+		return (1 + _strlen_recursion(s + 1));
 }
