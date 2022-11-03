cb4369469fbcba6af483b951feeb6f149001a5d8
MsGeeO <geethatcher@gmail.com>
2022-11-03 12:04:48 -0700

    update

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -5,12 +5,15 @@
  * @dest: is destination memory
  * @src: is source memory
  * @n: number of bytes to be copied
- * Return: string copied from source
+ * Return: dest
  */
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
 	unsigned int a;
 
 	for (a = 0; a < n; a++)
+	{
 		dest[a] = src[a];
+	}
 	return (dest);
+}
