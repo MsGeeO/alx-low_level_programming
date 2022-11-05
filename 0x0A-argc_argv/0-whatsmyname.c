bbba1f3ea72e400a26dc97bbc9b8b76c99f61a21
MsGeeO <geethatcher@gmail.com>
2022-11-05 09:46:47 -0700

    update

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -8,7 +8,7 @@
  * @argv: pointer that contains the arrys of the arguments
  * Return: 0
  */
-int main(int argc __attribute__((unused)), char *argv[])
+int main(int argc __attribute__((unused)) char *argv[])
 {
 	printf("%/s\n", argv[0]);
 	return (0);
