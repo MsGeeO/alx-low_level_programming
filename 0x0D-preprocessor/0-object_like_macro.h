ef898eb2af65557aeca701dd9b9351fcb7c5d4a4
MsGeeO <geethatcher@gmail.com>
2022-11-15 12:35:34 -0800

    text

---
+++ 0x0D-preprocessor/0-object_like_macro.h
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * object_like_macro.h - creates a header file that defines a macro
+ * @size: name of macro
+ * @1024: token abbreviation
+ * 
+ * Return: Always 0
+ */
+int main(void)
+{
+#define SIZE 1024
+}
