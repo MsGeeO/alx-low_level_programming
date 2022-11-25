4f52a7154d80bd38ac8308c3486692660953dbf2
Glory Ogbondah <geethatcher@gmail.com>
2022-11-25 22:04:52 +0000

    update

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -8,9 +8,9 @@
 
 size_t print_list(const list_t *h)
 {
-	size_t bulie;
+	size_t nelem;
 
-	bulie = 0;
+	nelem = 0;
 	while (h != NULL)
 	{
 		if (h->str == NULL)
@@ -18,7 +18,7 @@ size_t print_list(const list_t *h)
 		else
 			printf("[%d] %s\n", h->len, h->str);
 		h = h->next;
-		bulie++;
+		nelem++;
 	}
-	return (bulie);
+	return (nelem);
 }
