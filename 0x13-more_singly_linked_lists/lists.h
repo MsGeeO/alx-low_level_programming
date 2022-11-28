0a7f3279971315737afe10e6b98227f9e1d97fa9
Glory Ogbondah <geethatcher@gmail.com>
2022-11-28 17:53:30 +0000

    update

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -8,7 +8,6 @@
  * struct listint_s - singly linked list
  * @n: integer
  * @next: points to the next node
- * 
  * Description: singly linked list node structure
  * for Holberton project
  */
@@ -22,7 +21,6 @@ typedef struct listint_s
  * struct listp_s - singly linked list
  * @p: pointers of nodes
  * @next: points to the next node
- * 
  * Description: singly linked list of pointers
  */
 typedef struct listp_s
